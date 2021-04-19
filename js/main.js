fa71ffb6ce0ac5a13f508844f2ba893a9e8149f4
Kevin <kevinabouhanna@gmail.com>
2021-04-19 18:32:33 +0300

    change the title when the user moves to another tab

--- js/main.js
+++ js/main.js
@@ -24,3 +24,6 @@ if (document.body) {
     }
 }
 
+// change the title when the user moves to another tab
+window.onblur = function () { document.title = '❤️ Please come back!'; }
+window.onfocus = function () { document.title = 'Kevin Abou Hanna'; }
