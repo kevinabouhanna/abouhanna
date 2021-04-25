9991c51b400545bbf80a4a91b96a11249079f4a4
Kevin <kevinabouhanna@gmail.com>
2021-04-25 12:50:56 +0300

    fixed JavaScript ordering

--- js/main.js
+++ js/main.js
@@ -1,19 +1,15 @@
+//remove .html from url
+var url = window.location.href;
+url = url.split('index.html')[0];
+window.history.replaceState(null, null, url);
+
 // Body styling and greeting message based on local time
 // different greeting messages based on local time
 const myDate = new Date();
-const helloHand = '<span class="hello"> 👋🏼</span>';
 let hrs = myDate.getHours();
+const helloHand = '<span class="hello"> 👋🏼</span>';
 
 let greet;
-
-if (hrs >= 4 && hrs < 12)
-    greet = 'Good Morning' + helloHand;
-else if (hrs >= 12 && hrs <= 17)
-    greet = 'Good Afternoon' + helloHand;
-else
-    greet = 'Good Evening' + helloHand;
-
-document.getElementById('greetingHuman').innerHTML = greet;
 
 // add day or night class to body based on local time
 if (document.body) {
@@ -23,6 +19,15 @@ if (document.body) {
         document.body.className = "dark";
     }
 }
+
+if (hrs >= 4 && hrs < 12)
+    greet = 'Good Morning' + helloHand;
+else if (hrs >= 12 && hrs <= 17)
+    greet = 'Good Afternoon' + helloHand;
+else
+    greet = 'Good Evening' + helloHand;
+
+document.getElementById('greetingHuman').innerHTML = greet;
 
 // change the title when the user moves to another tab
 window.onblur = function () { document.title = 'Please come back 🥺'; }
