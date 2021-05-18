efcadd4e7803e3ffc846edba2e16b9e6a7f0cade
Kevin <kevinabouhanna@gmail.com>
2021-05-18 14:46:05 +0300

    updated title

--- js/main.js
+++ js/main.js
@@ -36,4 +36,4 @@ if (document.body) {
 
 // change the title when the user moves to another tab
 window.onblur = function () { document.title = 'Please come back 🥺'; }
-window.onfocus = function () { document.title = 'Kevin Abou Hanna'; }
+window.onfocus = function () { document.title = 'Kevin Abou Hanna - Product Designer and Frontend Developer'; }
