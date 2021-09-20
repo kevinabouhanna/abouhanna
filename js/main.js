61da2d1e13030328f7d85bcd0c46a45cc924ec94
Kevin <kevinabouhanna@gmail.com>
2021-09-20 16:40:37 +0300

    Using BEM Methodology

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // show focus only when user is tabbing, you can find it's relevant CSS code in main.css
 function handleFirstTab(e) {
     if (e.keyCode === 9) { // the "I am a keyboard user" key
-        document.body.classList.add('user-is-tabbing');
+        document.body.classList.add('user__is__tabbing');
         window.removeEventListener('keydown', handleFirstTab);
     }
 }
