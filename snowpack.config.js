8bbefc61de32bd132f981e4262542d1d469a7a1f
Kevin <kevinabouhanna@gmail.com>
2021-09-26 04:35:44 +0300

    updated Snowpack & Netlify configurations

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,5 +5,15 @@ module.exports = {
   plugins: [
     '@snowpack/plugin-babel',
     '@snowpack/plugin-postcss',
-  ]
+  ],
+  optimize: {
+    /* Use built-in esbuild bundling for build */
+    bundle: true,
+    minify: true,
+    target: "es2020",
+    treeshake: true,
+    splitting: true,
+    // Additional opts:
+    // https://www.snowpack.dev/guides/optimize-and-bundle#option-1%3A-built-in-optimizations
+  }
 };
