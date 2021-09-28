76fbdc0ac6e7c3631cefa93677738742baaec2ca
Kevin <kevinabouhanna@gmail.com>
2021-09-29 02:18:50 +0300

    testing postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,7 @@
-const postcssPresetEnv  = require('postcss-preset-env')
-const postcssImport     = require('postcss-import')
-const postcsseasings    = require('postcss-easings')
-const importUrl         = require('postcss-import-url')
-
 module.exports = {
   plugins: [
-    importUrl(),
-    postcssImport({
-      path: 'public/css',
-    }),
-    postcsseasings(),
-    postcssPresetEnv({
-      stage: 0,
-      features: {
-        'logical-properties-and-values': false,
-        'prefers-color-scheme-query': false,
-        'gap-properties': false,
-      }
-    }),
-  ]
-}
+    // Replace below with your plugins
+    require('cssnano'),
+    require('postcss-preset-env')
+  ],
+};
