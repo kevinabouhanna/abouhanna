6e0340675c0ddaf2ed374bf4e66ce8e991d299a2
Gaby Karam <info@gabykaram.com>
2021-12-23 14:11:07 +0200

    Adding Parralax footer

--- public/js/main.js
+++ public/js/main.js
@@ -36,3 +36,73 @@ if (document.body) {
 // change the title when the user moves to another tab
 window.onblur = function () { document.title = 'Please come back 🥺'; }
 window.onfocus = function () { document.title = 'Kevin Abou Hanna - Product Designer and Frontend Developer'; }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const placeholder = document.querySelector('.parallax-placeholder')
+  const footer = document.querySelector('footer-component')
+  let placeholderTop,
+    ticking
+
+  window.addEventListener('resize', onResize)
+
+  // On DOM Content Load, set placeholder height to be equal to footer height
+  updateHolderHeight()
+  checkFooterHeight()
+  let intervalIndex = 0;
+  const myInterval = setInterval(updateFooterWidth, 250)
+  // On window resize, update placeholder height to be equal to footer height
+  function onResize() {
+    updateHolderHeight()
+    checkFooterHeight()
+    updateFooterWidth()
+  }
+
+  function updateFooterWidth() {
+    if (intervalIndex > 6) {
+      clearInterval(myInterval);
+    }
+    var scrollDiv = document.body.querySelector('.parallax-container');
+    var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+    footer.style.width = `calc(100% - ${scrollbarWidth}px)`;
+    intervalIndex++;
+  }
+
+  // Placeholder should always match footer height
+  function updateHolderHeight() {
+    placeholder.style.height = `${footer.offsetHeight}px`
+  }
+
+  function checkFooterHeight() {
+    if (footer.offsetHeight > window.innerHeight) { // Check if footer is taller than window height
+      window.addEventListener('scroll', onScroll)
+      footer.style.bottom = 'unset'
+      footer.style.top = '0px'
+    } else { // If footer height is not greater than window height, bottom is 0 for normal parllax 
+      window.removeEventListener('scroll', onScroll)
+      footer.style.top = 'unset'
+      footer.style.bottom = '0px'
+    }
+  }
+
+  function onScroll() {
+    placeholderTop = Math.round(placeholder.getBoundingClientRect().top)
+    requestTick()
+  }
+
+  function requestTick() {
+    if (!ticking) requestAnimationFrame(updateBasedOnScroll)
+    ticking = true
+  }
+
+  function updateBasedOnScroll() {
+    // Reset the tick so we can capture the next onScroll
+    ticking = false
+
+    // When main content disappears from view, start to move footer up 
+    // in conjunction with placeholder top value (in relation to viewport)
+    if (placeholderTop <= 0) {
+      footer.style.top = `${placeholderTop}px` // match footer top value with placeholder's top value
+    }
+  }
+})
