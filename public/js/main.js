214055d4b03809b4d085262518968bdf5fdcd52c
Kevin <kevin@abouhanna.com>
2025-01-16 17:17:34 +0200

    updated Resume and removed a petty line of JS

--- public/js/main.js
+++ public/js/main.js
@@ -33,11 +33,6 @@ if (document.body) {
   }
 }
 
-// change the title when the user moves to another tab
-window.onblur = function () { document.title = 'Please come back 🥺'; }
-window.onfocus = function () { document.title = 'Kevin Abou Hanna - Product Designer and Frontend Developer'; }
-
-
 document.addEventListener("DOMContentLoaded", function () {
   const placeholder = document.querySelector('.parallax-placeholder')
   const footer = document.querySelector('.footer-container')
@@ -67,7 +62,7 @@ document.addEventListener("DOMContentLoaded", function () {
       window.addEventListener('scroll', onScroll)
       footer.style.bottom = 'unset'
       footer.style.top = '0px'
-    } else { // If footer height is not greater than window height, bottom is 0 for normal parllax 
+    } else { // If footer height is not greater than window height, bottom is 0 for normal parllax
       window.removeEventListener('scroll', onScroll)
       footer.style.top = 'unset'
       footer.style.bottom = '0px'
@@ -88,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Reset the tick so we can capture the next onScroll
     ticking = false
 
-    // When main content disappears from view, start to move footer up 
+    // When main content disappears from view, start to move footer up
     // in conjunction with placeholder top value (in relation to viewport)
     if (placeholderTop <= 0) {
       footer.style.top = `${placeholderTop}px` // match footer top value with placeholder's top value
